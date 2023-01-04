b6a2881e942a2f1a79f664f1050ba7ee040d4395
Harun Can Yildirim <eharuncan@gmail.com>
2023-01-04 12:32:29 +0300

    kurban ekleme ve bilgi sayfalarındaki hatalar düzeltildi.

--- client/src/app/components/kurban-bilgi/kurban-bilgi.component.ts
+++ client/src/app/components/kurban-bilgi/kurban-bilgi.component.ts
@@ -7,67 +7,117 @@ import {KurbanService} from '../../services/kurban.service';
 import {KURBAN} from "../../mock-data";
 import {Cins} from "../../enums/cins";
 import {KunyeBuyukbas, KunyeKucukbas} from "../../enums/kunye";
+import {Durum} from "../../enums/durum";
 
 @Component({
-  selector: 'app-kurban-bilgi',
-  templateUrl: './kurban-bilgi.component.html',
-  styleUrls: [ './kurban-bilgi.component.css' ]
+    selector: 'app-kurban-bilgi',
+    templateUrl: './kurban-bilgi.component.html',
+    styleUrls: ['./kurban-bilgi.component.css']
 })
 export class KurbanBilgiComponent implements OnInit {
-  kurban: Kurban = KURBAN;
+    // kurban: Kurban = KURBAN;
+    kurban: Kurban = {
+        id: 0,
+        resimUrl: "",
+        cins: Cins.KUCUKBAS,
+        kunye: KunyeKucukbas.KOYUN,
+        kupeNo: "123",
+        kilo: 0,
+        yas: 0,
+        fiyat: 0,
+        durum: Durum.SATISTA,
+        kesimSirasi: 0,
+        hisseAdedi: 0,
+        hisseList: []
+    };
+    kurbanResimUrl: string = "";
+    cinsler: string [] = [];
+    kunyeler: string [] = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private kurbanService: KurbanService,
-    private location: Location
-  ) {}
-
-  ngOnInit(): void {
-    this.getKurban();
-
-    if (this.kurban.cins === Cins.KUCUKBAS){
-      this.kunyeler = Object.values(KunyeKucukbas);
-    }else if (this.kurban.cins === Cins.BUYUKBAS){
-      this.kunyeler = Object.values(KunyeBuyukbas);
+    constructor(
+        private route: ActivatedRoute,
+        private kurbanService: KurbanService,
+        private location: Location
+    ) {
     }
 
-    if (this.kurban.resimUrl == ""){
-      if(this.kurban.cins === Cins.KUCUKBAS){
-        this.kurban.resimUrl = "assets/images/kurban-kucukbas-default.png";
-      }else if (this.kurban.cins === Cins.BUYUKBAS){
-        this.kurban.resimUrl = "assets/images/kurban-buyukbas-default.png";
-      }
+    ngOnInit(): void {
+        this.getKurban();
     }
-  }
 
-  cinsler = Object.values(Cins);
-  kunyeler: string [] = [];
-
-  onCinsChange(secilenCins: { value: any; }) {
-    if (secilenCins.value === Cins.KUCUKBAS){
-      this.kunyeler = Object.values(KunyeKucukbas);
-    }else if (secilenCins.value === Cins.BUYUKBAS){
-      this.kunyeler = Object.values(KunyeBuyukbas);
+    onCinsChange(secilenCins: { value: any; }) {
+        if (secilenCins.value === Cins.KUCUKBAS) {
+            this.kunyeler = Object.values(KunyeKucukbas);
+            if (this.kurban.resimUrl == "") {
+                this.kurbanResimUrl = "assets/images/kurban-kucukbas-default.png";
+            } else {
+                this.kurbanResimUrl = this.kurban.resimUrl || "";
+            }
+        } else if (secilenCins.value === Cins.BUYUKBAS) {
+            this.kunyeler = Object.values(KunyeBuyukbas);
+            console.log(this.kurban.resimUrl);
+            if (this.kurban.resimUrl == "") {
+                console.log("buraya girdi");
+                this.kurbanResimUrl = "assets/images/kurban-buyukbas-default.png";
+            } else {
+                this.kurbanResimUrl = this.kurban.resimUrl || "";
+            }
+        }
     }
-  }
 
-  getKurban(): void {
-    this.kurban = KURBAN;
-    // const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
-    // this.kurbanService.getKurban(id)
-    //   .subscribe(kurban => this.kurban = kurban);
-  }
-
-  goBack(): void {
-    this.location.back();
-  }
-
-  save(): void {
-    if (this.kurban) {
-      this.kurbanService.updateKurban(this.kurban)
-        .subscribe(() => this.goBack());
+    onResimUrlChange(value: any) {
+        if (value == "") {
+            if (this.kurban.cins === Cins.KUCUKBAS) {
+                this.kurbanResimUrl = "assets/images/kurban-kucukbas-default.png";
+            } else if (this.kurban.cins === Cins.BUYUKBAS) {
+                this.kurbanResimUrl = "assets/images/kurban-buyukbas-default.png";
+            }
+        } else {
+            this.kurbanResimUrl = value;
+            this.kurban.resimUrl = value;
+        }
     }
-  }
+
+    getKurban(): void {
+        // this.kurban = KURBAN;
+        const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+        this.kurbanService.getKurban(id)
+            .subscribe(kurban => {
+                this.kurban = kurban;
+                this.change();
+            });
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+
+    save(): void {
+        if (this.kurban) {
+            this.kurbanService.updateKurban(this.kurban)
+                .subscribe(() => this.goBack());
+        }
+    }
+
+    change(): void {
+        this.cinsler = Object.values(Cins);
+
+        if (this.kurban.cins === Cins.KUCUKBAS) {
+            this.kunyeler = Object.values(KunyeKucukbas);
+        } else if (this.kurban.cins === Cins.BUYUKBAS) {
+            this.kunyeler = Object.values(KunyeBuyukbas);
+        }
+
+        if (this.kurban.resimUrl == "") {
+            if (this.kurban.cins === Cins.KUCUKBAS) {
+                this.kurbanResimUrl = "assets/images/kurban-kucukbas-default.png";
+            } else if (this.kurban.cins === Cins.BUYUKBAS) {
+                this.kurbanResimUrl = "assets/images/kurban-buyukbas-default.png";
+            }
+        } else {
+            this.kurbanResimUrl = this.kurban.resimUrl || "";
+        }
+    }
 }
 
 
--- client/src/app/components/kurban-ekle/kurban-ekle.component.ts
+++ client/src/app/components/kurban-ekle/kurban-ekle.component.ts
@@ -8,45 +8,73 @@ import {KurbanCreate} from "../../models/kurbanCreate";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-kurban-ekle',
-  templateUrl: './kurban-ekle.component.html',
-  styleUrls: ['./kurban-ekle.component.css']
+    selector: 'app-kurban-ekle',
+    templateUrl: './kurban-ekle.component.html',
+    styleUrls: ['./kurban-ekle.component.css']
 })
-export class KurbanEkleComponent implements OnInit{
+export class KurbanEkleComponent implements OnInit {
 
-  newKurban : KurbanCreate = {resimUrl: "assets/images/kurban-kucukbas-default.png", kupeNo: "", cins: Cins.KUCUKBAS, kunye: KunyeKucukbas.KOYUN, fiyat:0, yas:0, kilo: 0}
-
-  cinsler = Object.values(Cins);
-  kunyeler: string [] = [];
-
-  constructor(private router: Router, private kurbanService: KurbanService) {
-  }
-
-  ngOnInit(): void {
-    if (this.newKurban.cins === Cins.KUCUKBAS){
-      this.kunyeler = Object.values(KunyeKucukbas);
-    }else if (this.newKurban.cins === Cins.BUYUKBAS){
-      this.kunyeler = Object.values(KunyeBuyukbas);
+    newKurban: KurbanCreate = {
+        resimUrl: "",
+        kupeNo: "",
+        cins: Cins.KUCUKBAS,
+        kunye: KunyeKucukbas.KOYUN,
+        fiyat: 0,
+        yas: 0,
+        kilo: 0
     }
-  }
+    newKurbanResimUrl: string = "assets/images/kurban-kucukbas-default.png";
 
-  onCinsChange(secilenCins: { value: any; }) {
-    if (secilenCins.value === Cins.KUCUKBAS){
-      this.kunyeler = Object.values(KunyeKucukbas);
-    }else if (secilenCins.value === Cins.BUYUKBAS){
-      this.kunyeler = Object.values(KunyeBuyukbas);
+    cinsler = Object.values(Cins);
+    kunyeler: string [] = [];
+
+    constructor(private router: Router, private kurbanService: KurbanService) {
     }
-  }
 
-  onResimUrlChange(value: any) {
-    this.newKurban.resimUrl = value;
-  }
+    ngOnInit(): void {
+        if (this.newKurban.cins === Cins.KUCUKBAS) {
+            this.kunyeler = Object.values(KunyeKucukbas);
+        } else if (this.newKurban.cins === Cins.BUYUKBAS) {
+            this.kunyeler = Object.values(KunyeBuyukbas);
+        }
+    }
 
-  onSubmit(): void {
-    this.kurbanService.addKurban(this.newKurban)
-        .subscribe(kurban => {
-        });
-    this.router.navigate(['/kurbanlar']);
-  }
+    onCinsChange(secilenCins: { value: any; }) {
+        if (secilenCins.value === Cins.KUCUKBAS) {
+            this.kunyeler = Object.values(KunyeKucukbas);
+            if (this.newKurban.resimUrl == "") {
+                this.newKurbanResimUrl = "assets/images/kurban-kucukbas-default.png";
+            } else {
+                this.newKurbanResimUrl = this.newKurban.resimUrl || "";
+            }
+        } else if (secilenCins.value === Cins.BUYUKBAS) {
+            this.kunyeler = Object.values(KunyeBuyukbas);
+            if (this.newKurban.resimUrl == "") {
+                this.newKurbanResimUrl = "assets/images/kurban-buyukbas-default.png";
+            } else {
+                this.newKurbanResimUrl = this.newKurban.resimUrl || "";
+            }
+        }
+    }
+
+    onResimUrlChange(value: any) {
+        if (value == "") {
+            if (this.newKurban.cins === Cins.KUCUKBAS) {
+                this.newKurbanResimUrl = "assets/images/kurban-kucukbas-default.png";
+            } else if (this.newKurban.cins === Cins.BUYUKBAS) {
+                this.newKurbanResimUrl = "assets/images/kurban-buyukbas-default.png";
+            }
+        } else {
+            this.newKurbanResimUrl = value;
+            this.newKurban.resimUrl = value;
+        }
+    }
+
+    onSubmit(): void {
+        this.kurbanService.addKurban(this.newKurban)
+            .subscribe(kurban => {
+              this.router.navigate(['/kurbanlar']);
+            });
+    }
 
 }
